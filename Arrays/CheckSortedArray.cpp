ab3015d9a770c72bf4247795359c403df08ebe54
Nishita Rawat <rawatnishita404@gmail.com>
2026-06-09 12:04:37 +0530

    Solved -  Check if Array is Sorted

---
+++ Arrays/CheckSortedArray.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool is_sorted(vector<int> arr, int n){
+    for(int i = 0; i < n-1; i++){
+        if(arr[i] < arr[i+1]){
+            continue;
+        }else{
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    vector<int> arr = {12, 34, 45, 67, 89};
+    int n = arr.size();
+    
+    if(is_sorted(arr, n)){
+        cout << "The array is sorted in increasing order." << endl;
+    }else{
+        cout << "The array is not sorted in increasing order." << endl;
+    }
+
+    return 0;
+}
