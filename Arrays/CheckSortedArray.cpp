3da0919dc0bc2ea2fc608282cd1cd71fc6a58ff9
Nishita Rawat <rawatnishita404@gmail.com>
2026-06-09 12:12:02 +0530

    Modified solution for Check if an Array is Sorted

--- Arrays/CheckSortedArray.cpp
+++ Arrays/CheckSortedArray.cpp
@@ -1,11 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool is_sorted(vector<int> arr, int n){
+bool is_sorted(const vector<int> &arr, int n){
     for(int i = 0; i < n-1; i++){
-        if(arr[i] < arr[i+1]){
-            continue;
-        }else{
+        if(arr[i] > arr[i+1]){
             return false;
         }
     }
