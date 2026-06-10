99b6c46c1960bdac559ca7be5e921571aa4c5093
Nishita Rawat <rawatnishita404@gmail.com>
2026-06-10 10:26:32 +0530

    Arrays: Remove Duplicates From Sorted Array

---
+++ Arrays/RemoveDuplicateElement.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Remove Duplicate elements from a sorted array and return the last unique element.
+
+int remove_duplicates(vector<int> &v, int n){
+    int st = 0;
+    int end = 1;
+    while(end < n){
+        if(v[end] == v[st]){
+           end++;
+        }else{
+            st++;
+            v[st] = v[end];
+            end++;
+        }
+    }
+    return st;
+}
+
+int main(){
+    vector<int> v = {1, 1, 2, 2, 3, 4, 4, 5};
+    int n = v.size();
+
+    cout <<  "Before removing duplicates:";
+    for(int x :v ){
+        cout << x << " ";
+    }
+    cout << endl;
+
+    cout << "The Last Unique Element is :" << remove_duplicates(v, n) << endl;
+    cout << endl;
+
+    return 0;
+}
