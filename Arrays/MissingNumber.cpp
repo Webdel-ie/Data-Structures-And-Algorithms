9066f740d26afa6044282dcea6304db9e690a825
Nishita Rawat <rawatnishita404@gmail.com>
2026-06-13 11:31:34 +0530

    Added - Optimal Solution for Missing Number problem

--- Arrays/MissingNumber.cpp
+++ Arrays/MissingNumber.cpp
@@ -1,26 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int missing_no(vector<int> &v, int n){
-    for(int i = 1; i <= n; i++){
-        int flag = 0;
+//BRUTE FORCE APPROACH--------------------------------------------------------------------------
+// TC = O(n*n)
+// SC = o(1)
 
-        for(int j = 0; j < n-1; j++){
-            if(v[j] == i){
-                flag = 1;
-                break;
-            }
-        }
 
-        if(flag == 0){
-            return i;
-        }
+// BETTER APPROACH------------------------------------------------------------------------------
+// TC = O(2n)
+// SC = O(2n)
+
+//OPTIMAL APPROACH------------------------------------------------------------------------------
+//  1. Sum Method
+// TC = O(n)
+// SC = O(1)
+
+int missing_no(const vector<int> v, int n){
+    int sum = 0; 
+    int s = 0;
+    
+    sum = (n * (n + 1) / 2);
+
+    for(int i = 0; i < v.size(); i++){
+       s += v[i];
     }
-    return -1;
+    return (sum - s);
 }
 
 int main(){
-    vector<int> v = {1, 2, 3, 4, 5, 6};
+    vector<int> v = {1, 2, 4, 5};
     int n = v.size() + 1;
 
     cout << "The missing number is: " << missing_no(v, n) << endl;
