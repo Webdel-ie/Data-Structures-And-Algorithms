f10fbce871d5f29c9b8aada6ea55f21582c31336
Nishita Rawat <rawatnishita404@gmail.com>
2026-06-14 19:50:30 +0530

    Added - Optimized Solution to Element Appearing Only Once Problem

--- Arrays/ElementAppearingOnce.cpp
+++ Arrays/ElementAppearingOnce.cpp
@@ -1,28 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// TC = O(2n)
+// Brute Force--------------------------------------------------
+// TC = O(n*n)
+// SC =O(1)
+
+// Optimal Approach---------------------------------------------
+
+// TC = O(n)
 // SC = O(1)
 
 int appear_once(const vector<int> &v, int n){
-    int s1 = 0;
-    int s2 = 0;
-    int x = (n+1)/2;
-
-    for(int i = 1; i <= x ; i++){
-       s1 += 2*i;
-    }
-
+    int xor1 = 0;
     for(int i = 0; i < n; i++){
-        s2 += v[i];
+        xor1 ^= v[i];
     }
-
-    return (s1 - s2);
-    
+    return xor1;
 }
 
 int main(){
-    vector<int> v = {1, 1, 2, 3, 3, 4, 4};
+    vector<int> v = {1, 1, 2, 2, 6, 7, 7, 3, 3, 4, 4};
     int n = v.size();
 
     cout << "The Element, that appears only once, is : " << appear_once(v, n) << endl;
