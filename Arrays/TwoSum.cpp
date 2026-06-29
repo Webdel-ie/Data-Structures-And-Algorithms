f83de129477c030d5cad13c17a49cc51516e22b4
Nishita Rawat <rawatnishita404@gmail.com>
2026-06-29 21:59:05 +0530

    Optimized code for 2 sums problem

--- Arrays/TwoSum.cpp
+++ Arrays/TwoSum.cpp
@@ -5,38 +5,30 @@ using namespace std;
 // SC = O(1)
 
 vector<int> two_sum(vector<int> &v, int target_sum){
-    int n = v.size();
-    int l = 0;
-    int r = n-1;
-    while(l <= r){
-       if(v[l] + v[r] == target_sum){
-        return {l, r};
-       }
-       else if(v[l] + v[r] > target_sum){
-        r--;
-       }
-       else{
-        l++;
-       }
+    int l = 0; 
+    int r = v.size()-1;
+    sort(v.begin(), v.end());
+    while(l < r){
+        int sum = v[l] + v[r];
+        if(sum == target_sum){
+            return {l, r};
+        }
+        else if(sum < target_sum)  l++;
+        else r--;
     }
     return {};
 }
 
 int main(){
-    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    vector<int> v = {2, 5, 6, 8, 11};
     int target_sum;
 
     cout << "Enter the target sum: " ;
     cin >> target_sum ;
 
     cout << "The target sum is found at indexes: ";
-    vector<int> res = two_sum(v, target_sum);
-    for(size_t i = 0; i < res.size(); ++i){
-        if(i) cout << ", ";
-        cout << res[i];
+    for(int x : two_sum(v, target_sum)){
+        cout << x << " ";
     }
-    if(res.empty()) cout << "None";
-
-
     return 0;
 }
