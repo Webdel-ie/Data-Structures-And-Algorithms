cc334792d53a93b7c95f2566dce369de36b4b202
Nishita Rawat <rawatnishita404@gmail.com>
2026-07-30 23:46:10 +0530

    Minimum Number of Pushes to Type Word I

---
+++ LeetCodes/LC3014.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Minimum Number of Pushes to Type Word I
+
+int minimumPushes(string word) {
+    int ans = 0;
+    int n = word.size();
+
+    for (int i = 0; i < n; i++) {
+        ans += (i / 8) + 1;
+    }
+
+    return ans;
+}
+
+int main(){
+    string word = "abcde";
+ 
+    cout << "the minimum number of pushes needed to type word after remapping the keys : " << minimumPushes(word) << endl;
+    return 0;
+}
