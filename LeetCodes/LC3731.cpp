c981c78020d6dd501336c7d55573ad72a3d8e01d
Nishita Rawat <rawatnishita404@gmail.com>
2026-08-04 23:54:01 +0530

    Update LC3731.cpp
    Find Missing Elements

--- LeetCodes/LC3731.cpp
+++ LeetCodes/LC3731.cpp
@@ -1,26 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 // Find Missing Elements 
 
-class Solution {
-public:
-    vector<int> findMissingElements(vector<int>& nums) {
-        vector<int> ans;
+vector<int> findMissingElements(vector<int>& nums) {
+    vector<int> ans;
 
-        sort(nums.begin(), nums.end());
+    sort(nums.begin(), nums.end());
 
-        int small = nums[0];
-        int large = nums.back();
+    int small = nums[0];
+    int large = nums.back();
 
-        int i = 0;
+    int i = 0;
 
-        while (small <= large) {
-            if (i < nums.size() && nums[i] == small) {
-                i++;
-            } else {
-                ans.push_back(small);
-            }
-            small++;
+    while (small <= large) {
+        if (i < nums.size() && nums[i] == small) {
+            i++;
+        } else {
+            ans.push_back(small);
         }
-
-        return ans;
+        small++;
     }
-};
+
+     return ans;
+}
+
+int main(){
+    vector<int> nums = {5, 1};
+
+    cout << "The missing elements are: ";
+    for(auto x : findMissingElements(nums)){
+        cout << x << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
